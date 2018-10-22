8b8279eba94b11a5e84b9038f2acf7d3e285c227
phebert5009 <cpierrot273@gmail.com>
2018-10-22 17:55:33 -0600

    added sample_files

---
+++ sample_programs/sample_client.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3       
+# RUN ON BRICK
+    
+import socket
+
+def setup_client(host, port):
+	# create a socket object
+	print("setting up client")
+	s = socket.socket(socket.AF_INET, socket.SOCK_STREAM) 
+	# connect to hostname on the port.
+	s.connect((host, port))                               
+	# Receive no more than 1024 bytes
+	while True:
+		msg = s.recv(1024) 
+		if len(msg) > 0:
+			print("Received: ", msg)
+		else:
+			break
+	s.close()
+
+if __name__ == "__main__":
+	host = socket.gethostname()                           
+	port = 9999
+	setup_client(host, port)
---
+++ sample_programs/sample_server.py
@@ -0,0 +1,40 @@
+#!/usr/bin/python
+# RUN ON LAPTOP USING PYTHON 2.7
+
+import socket                                         
+import cv2
+
+def capture_frames(clientsocket):
+	cv2.namedWindow("webcam")
+	vc = cv2.VideoCapture(1)
+	if vc.isOpened(): # try to get the first frame
+		rval, frame = vc.read()
+	else:
+		rval = False
+	i = 0
+	while rval:
+		# handle current frame
+		cv2.imshow("webcam", frame)
+		rval, frame = vc.read()
+		# send calculated information to ev3
+		clientsocket.send(str(i))
+		i+=1
+		# check if esc key pressed
+		key = cv2.waitKey(20)
+		if key == 27:
+			clientsocket.close()
+			cv2.destroyWindow("webcam")
+			break
+
+if __name__ == "__main__":
+	# setup server socket
+	serversocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM) 
+	host = socket.gethostname()   
+	print "host: ", host                         
+	port = 9999
+	serversocket.bind((host, port))                                  
+	# queue up to 5 requests
+	serversocket.listen(5) 
+	clientsocket,addr = serversocket.accept()  
+	print "Connected to: %s" % str(addr) 
+	capture_frames(clientsocket)
