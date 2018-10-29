64097b9930d3ac980fc4a1bac7c183f449aeaad8
Pineros Rhon <pierosrh@registrar.ualberta.ca>
2018-10-29 17:17:39 -0600

    Removed testing files.

--- sample_programs/visual_servoing.py
+++ sample_programs/visual_servoing.py
@@ -1,71 +1,98 @@
 import cv2
-import numpy as np
-#For Red
-# lower mask (0-10)
-redLowerLowMask = (0,50,50)
-redUpperLowMask = (10,255,255)
-
-# high mask (170-180)
-redLowerHighMask = (170,50,50)
-redUpperHighMask = (180,255,255)
-
-# for blue
-blueLowerLowMask = (100,150,0)
-blueUpperLowMask = (140,255,255)
-
-cv2.namedWindow("webcam")
-vc = cv2.VideoCapture(0)
-
-
-if vc.isOpened(): # try to get the first frame
-    rval, frame = vc.read()
-else:
-    rval = False
-i = 0
-while rval:
-    # handle current frame
-
-    rval, frame = vc.read()
-    #blurred = cv2.GaussianBlur(frame, (11, 11), 0)
-    blurred = cv2.medianBlur(frame,11)
-    hsv = cv2.cvtColor(blurred, cv2.COLOR_BGR2HSV)
-    #lowMask = cv2.inRange(hsv, redLowerLowMask, redUpperLowMask)
-    #highMask = cv2.inRange(hsv, redLowerHighMask, redUpperHighMask)
-    #mask =   highMask + lowMask 
-    mask = cv2.inRange(hsv, blueLowerLowMask, blueUpperLowMask)
-    kernel = cv2.getStructuringElement(cv2.MORPH_ELLIPSE, (5,5))
-    mask = cv2.erode(mask, np.ones((11, 11),np.uint8), iterations=2)
-    mask = cv2.dilate(mask, np.ones((11, 11),np.uint8), iterations=5)
-    res = cv2.bitwise_and(blurred,blurred, mask= mask)
-    res2 = cv2.cvtColor(res, cv2.COLOR_BGR2GRAY)
-    gray_res =  cv2.cvtColor(blurred, cv2.COLOR_BGR2GRAY)
-    # detect circles in the image
-    circles = cv2.HoughCircles(res2, cv2.HOUGH_GRADIENT, 1.5, 300, param1=100, param2=20, minRadius=20, maxRadius=200)
-    
-    # ensure at least some circles were found
-    if circles is not None:
-        # convert the (x, y) coordinates and radius of the circles to integers
-        circles = np.round(circles[0, :]).astype("int")
-        # loop over the (x, y) coordinates and radius of the circles
-        for (x, y, r) in circles:
-            print("Circles")
-            # draw the circle in the output image, then draw a rectangle
-            # corresponding to the center of the circle
-            cv2.circle(frame, (x, y), r, (0, 255, 0), 4)
-            cv2.rectangle(frame, (x - 5, y - 5), (x + 5, y + 5), (0, 128, 255), -1)
+import sys
  
-	# show the output image
-    cv2.imshow("output", res2)
-    
-    cv2.imshow("webcam", frame)
-    cv2.imshow("blurred", blurred)
-    cv2.imshow("mask", mask)
-    cv2.imshow("and", res)
+(major_ver, minor_ver, subminor_ver) = (cv2.__version__).split('.')
+ 
+print(cv2.__version__)
+
+if __name__ == '__main__' :
+ 
+    # Set up tracker.
+    # Instead of MIL, you can also use
+ 
+    tracker_types = ['BOOSTING', 'MIL','KCF', 'TLD', 'MEDIANFLOW', 'GOTURN', 'MOSSE', 'CSRT']
+    tracker_type = tracker_types[2]
+ 
+    if int(minor_ver) < 3:
+        tracker = cv2.Tracker_create(tracker_type)
+    else:
+        if tracker_type == 'BOOSTING':
+            tracker = cv2.TrackerBoosting_create()
+        if tracker_type == 'MIL':
+            tracker = cv2.TrackerMIL_create()
+        if tracker_type == 'KCF':
+            tracker = cv2.TrackerKCF_create()
+        if tracker_type == 'TLD':
+            tracker = cv2.TrackerTLD_create()
+        if tracker_type == 'MEDIANFLOW':
+            tracker = cv2.TrackerMedianFlow_create()
+        if tracker_type == 'GOTURN':
+            tracker = cv2.TrackerGOTURN_create()
+        if tracker_type == 'MOSSE':
+            tracker = cv2.TrackerMOSSE_create()
+        if tracker_type == "CSRT":
+            tracker = cv2.TrackerCSRT_create()
+ 
+    tracker = cv2.TrackerBoosting_create()
+    cv2.namedWindow("webcam")
+    vc = cv2.VideoCapture(1)
 
 
-    i+=1
-    # check if esc key pressed
-    key = cv2.waitKey(20)
-    if key == 27:
-        cv2.destroyWindow("webcam")
-        break
+    if vc.isOpened(): # try to get the first frame
+        rval, frame = vc.read()
+    else:
+        rval = False
+
+    # Define an initial bounding box
+    #bbox = (287, 23, 86, 320)
+    i = 0
+    while rval and i < 100:
+        # Read a new frame
+        rval, frame = vc.read()
+        cv2.imshow("webcam", frame)
+        i = i + 1
+
+ 
+    # Uncomment the line below to select a different bounding box
+    bbox = cv2.selectROI("webcam", frame, False)
+
+
+ 
+    # Initialize tracker with first frame and bounding box
+    rval = tracker.init(frame, bbox)
+ 
+    while rval:
+        # Read a new frame
+        rval, frame = vc.read()
+ 
+        # Start timer
+        timer = cv2.getTickCount()
+ 
+        # Update tracker
+        ok, bbox = tracker.update(frame)
+ 
+        # Calculate Frames per second (FPS)
+        fps = cv2.getTickFrequency() / (cv2.getTickCount() - timer);
+ 
+        # Draw bounding box
+        if ok:
+            # Tracking success
+            p1 = (int(bbox[0]), int(bbox[1]))
+            p2 = (int(bbox[0] + bbox[2]), int(bbox[1] + bbox[3]))
+            cv2.rectangle(frame, p1, p2, (255,0,0), 2, 1)
+        else :
+            # Tracking failure
+            cv2.putText(frame, "Tracking failure detected", (100,80), cv2.FONT_HERSHEY_SIMPLEX, 0.75,(0,0,255),2)
+ 
+        # Display tracker type on frame
+        cv2.putText(frame, tracker_type + " Tracker", (100,20), cv2.FONT_HERSHEY_SIMPLEX, 0.75, (50,170,50),2);
+     
+        # Display FPS on frame
+        cv2.putText(frame, "FPS : " + str(int(fps)), (100,50), cv2.FONT_HERSHEY_SIMPLEX, 0.75, (50,170,50), 2);
+ 
+        # Display result
+        cv2.imshow("webcam", frame)
+ 
+        # Exit if ESC pressed
+        k = cv2.waitKey(1) & 0xff
+        if k == 27 : break
